a509cc8c2b369b40afc96ba27fd500e8d56a5e76
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-01 17:25:46 -0500

    Created repo

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,9 @@
+import string
+
 def caesar_cipher(string, shift_amount):
+    dictionary = dict.fromkeys(string.ascii_lowercase, 0)
+
+    for char in dictionary:
+        print(char)
+
+caesar_cipher("Boy! What a string!", -5)
